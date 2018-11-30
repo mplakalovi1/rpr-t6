29422c5bd98041138fa7f2be2b6798f18917bbd7
origin <mplakalovi1@etf.unsa.ba>
2018-11-30 13:26:23 +0100

    commit_v1

---
+++ src/ba/unsa/etf/rpr/tutorijal6/Main.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr.tutorijal6;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/formular.fxml"));
+        primaryStage.setTitle("Formular za unos podataka o studentu");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
